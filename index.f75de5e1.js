3bf43036ef8b7a975827e1388c7a33919ae82c89
SerhiiSemennikov <semennik@hotmail.com>
2024-11-03 23:45:47 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,32 @@
+"use strict";
+const body = document.querySelector("body");
+function waitFor(element, eventName) {
+    return new Promise((resolve, reject)=>{
+        if (!element) {
+            const error = `It was NOT ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+            reject(error);
+        }
+        element.addEventListener(eventName, ()=>{
+            resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+        });
+    });
+}
+const printMessage = (message)=>{
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.innerHTML = message;
+    body.appendChild(div);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
